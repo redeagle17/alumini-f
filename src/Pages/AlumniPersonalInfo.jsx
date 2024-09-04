37c4fd5787db2e4492694feefb442e98d0bfd230
redeagle17 <ankursingh1438@gmail.com>
2024-09-04 21:40:11 +0530

    profile photo fix

--- src/Pages/AlumniPersonalInfo.jsx
+++ src/Pages/AlumniPersonalInfo.jsx
@@ -45,10 +45,15 @@ function AlumniPersonalInfo() {
     const file = e.target.files[0];
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", process.env.CLOUDINARY_PRESET_NAME);
+    formData.append(
+      "upload_preset",
+      import.meta.env.VITE_CLOUDINARY_PRESET_NAME
+    );
     try {
       const res = await axios.post(
-        `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`,
         formData
       );
       setImage(res.data.secure_url);
